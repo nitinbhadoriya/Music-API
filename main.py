7edc9e3bb4281c51210c4fe9e599f33e976b3ef9
Nitin Bhadoriya <nbhadoriya@nbhador-ltmms15.internal.salesforce.com>
2021-06-16 10:14:22 +0530

    Add a function which will take parameters as arguments and will make the get request

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import requests
 
+
+def last_fm_requests(payload):
+    headers = {
+        'user-agent': USER_AGENT
+    }
+    url='https://ws.audioscrobbler.com/2.0/'
+    r = requests.get(url, headers = headers, params = payload)
+    return r.status_code
+
 API_KEY = 'c388e2286deda45d940563f3fae64a21'
 USER_AGENT = 'MusicAPI'
-
-headers = {
-    'user-agent': USER_AGENT
-}
-
 payload = {
     'api_key': API_KEY,
     'method': 'chart.gettopartists',
     'format': 'json'
 }
-
-r = requests.get('https://ws.audioscrobbler.com/2.0/', headers=headers, params=payload)
-print(r.status_code)
+print(last_fm_requests(payload))
